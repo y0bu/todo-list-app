f19ad419bb7388267aa1717ba23ef5058ee631ad
yoav_abu <vcxz3965@gmail.com>
2020-04-03 00:13:47 +0300

    commented all the class AdminController

--- src/main/java/com/yoav/todolist/controllers/AdminController.java
+++ src/main/java/com/yoav/todolist/controllers/AdminController.java
@@ -16,14 +16,22 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 
+/*
+* admin feature is medium size feature and it have the following operation:
+*     1) delete account
+*     2) delete task with specifying username
+*     3) see all the accounts and their password
+*     4) see all tasks of a specifying account
+*     5) add a new admin
+* */
 @Controller
 public class AdminController {
 
-    private TaskService taskService;
-    private AccountService accountService;
-    private AdminService adminService;
+    private TaskService taskService; // for deleting task operation
+    private AccountService accountService; // for seeing all the task of account and deleting account
+    private AdminService adminService; // for creating account and logging validation login
 
-    @Autowired
+    @Autowired // dependency injection on the services for singleton purposes
     public AdminController(TaskService taskService, AccountService accountService, AdminService adminService) {
         this.taskService = taskService;
         this.accountService = accountService;
@@ -31,11 +39,17 @@ public class AdminController {
         adminService.checkIfBaseAdminAccountExist();
     }
 
+    /*
+    * get a simple admin login page
+    * */
     @RequestMapping(value = "/admin/login", method = RequestMethod.GET)
     public String getLoginAdmin() {
         return "admin/login";
     }
 
+    /*
+    * check the credential of the admin login
+    * */
     @RequestMapping(value = "/admin/login", method = RequestMethod.POST, params = {"adminName", "password"})
     public String postValidateLoginAdmin(
             @RequestParam String adminName,
@@ -43,6 +57,11 @@ public class AdminController {
             Model model,
             HttpSession session) {
 
+        /*
+        * if the record with the specifying credential adminName and password are exist in the table "admins" grant
+        * permission (with HTTP session cookie) to the admin panel
+        * else display on the login page that the password or the adminName are incorrect
+        * */
         if (adminService.isExistByAdminNameAndPassword(adminName, password)) {
             session.setAttribute("isAdmin", true);
             return "redirect:/admin";
@@ -52,18 +71,33 @@ public class AdminController {
         }
     }
 
+    /*
+    * if the session not set(means that permission not granted to user to use admin panel) then redirect to unauthorized error
+    * else display list of all users and their info like passwords usernames and options like delete account
+    * and getting all the tasks of the user
+    * */
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String getAllAccountsMainAdminPanel(HttpSession session, Model model) {
         if (session.getAttribute("isAdmin") != null) {
             model.addAttribute("accounts", accountService.getAll());
+
+            // this line is for specifying the request path(to the account tasks management("/admin/{username}"))
+            // with the username of account
             model.addAttribute("urlToRedirect", "/admin/");
+
             return "admin/displayUsers";
         } else {
-            // TODO rise unauthorized error
             return "unauthorized";
         }
     }
 
+    /*
+    * on the very first line of code of the method we check if
+    * the user have permission to access that feature of deleting account
+    * if user have permission so user should pass parameter to that http post request with the variable name "deleteAccount"
+    * and that deleteAccount variable should contains the id of the account that we want to delete as string and then method
+    * delete the account and then we redirecting to the main page of the admin panel
+    * */
     @RequestMapping(value = "/admin", method = RequestMethod.POST, params = "deleteAccount")
     public String postDeleteAccount(@RequestParam String deleteAccount, HttpSession session) {
         if (session.getAttribute("isAdmin") == null) return "unauthorized";
@@ -72,6 +106,12 @@ public class AdminController {
         return "redirect:/admin";
     }
 
+    /*
+    * this mapping is simply but requesting a full list of tasks belong to the specifying username there is also a
+    * option to delete task with post request
+    * on the very first line of code of the method we check if
+    * the user have permission to access that feature of deleting account
+    * */
     @RequestMapping(value = "/admin/{usernameOfAccount}", method = RequestMethod.GET)
     public String getTasksOfNameOfAccounts(@PathVariable String usernameOfAccount, HttpSession session, Model model) {
         if (session.getAttribute("isAdmin") == null) return "unauthorized";
@@ -80,6 +120,11 @@ public class AdminController {
         return "admin/displayTasksOfUsers";
     }
 
+     /*
+     * on the very first line of code of the method we check if
+     * the user have permission to access that feature of deleting account
+     * this is post request to delete task by id. the id of the task that we want to delete we pass that as a string
+     * */
     @RequestMapping(value = "/admin/{usernameOfAccount}", method = RequestMethod.POST, params = "deleteTask")
     public String postDeleteTasksOfUsername(
             @RequestParam String deleteTask,
@@ -89,15 +134,27 @@ public class AdminController {
         if (session.getAttribute("isAdmin") == null) return "unauthorized";
         int idOfDeletingTask = Integer.parseInt(deleteTask);
         taskService.delete(idOfDeletingTask);
-        return "redirect:/admin/" + usernameOfAccount;
+        return "redirect:/admin/" + usernameOfAccount; // redirecting back to the list of tasks of the specifying username
     }
 
+    /*
+    * a simple get request to the page(simple form page) where we can add admins
+    * to add a admin you need to be admin so in the first line of code in the method we checking if the user that requesting
+    * the request is have permission for the admin feature
+    * */
     @RequestMapping(value = "/admin/add", method = RequestMethod.GET)
     public String getCreateNewAdmin(HttpSession session) {
         if (session.getAttribute("isAdmin") == null) return "unauthorized";
         else return "admin/createAdmin";
     }
 
+    /*
+    * this is post request of the form of the create admin page
+    * here we checking if the admin name is already exist because we cant allow two identical adminName is the admins table
+    * and then if there is no "adminName" in admins table we cant insert admin to the table admins
+    * else if there is "adminName" in table admins we returning back to the add page and then telling admin that you need to
+    * use other adminName because its already exist
+    * */
     @RequestMapping(value = "/admin/add", method = RequestMethod.POST, params = {"adminName", "password"})
     public String postAddAdmin(
             @RequestParam String adminName,
@@ -106,7 +163,7 @@ public class AdminController {
             Model model,
             RedirectAttributes attributes) {
 
-        if (session.getAttribute("isAdmin") == null) return "unauthorized";
+        if (session.getAttribute("isAdmin") == null) return "unauthorized"; // checking if user have permission to the admin feature
 
         if (adminService.isExistByAdminName(adminName)) {
             model.addAttribute("alert", "admin name is already have been taken");
@@ -115,34 +172,8 @@ public class AdminController {
             Admin newAdmin = new Admin(adminName, password);
             adminService.add(newAdmin);
             attributes.addFlashAttribute("alert", "the admin added successfully you can log in now");
-            return "redirect:/admin/login";
+            return "redirect:/admin/login"; // redirecting to login page because login was successfully done.
         }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
