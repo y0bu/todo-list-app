f0a0716498309cd07334d7eb156e47d363429343
yoav_abu <vcxz3965@gmail.com>
2020-04-16 21:35:12 +0300

    move the configuration to application.properties

--- src/main/java/com/yoav/todolist/config/ApplicationConfig.java
+++ src/main/java/com/yoav/todolist/config/ApplicationConfig.java
@@ -1,65 +1,13 @@
 package com.yoav.todolist.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories("com.yoav.todolist.dao")
 @ComponentScan("com.yoav.todolist")
 public class ApplicationConfig {
-
-    /*
-    * a session factory for hibernate operation
-    * */
-    @Bean(name = {"entityManagerFactory", "sessionFactory"})
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.yoav.todolist");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
-    }
-
-    /*
-    * the data source with the username and password and url and driver to make the connection to the database
-    * */
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUsername("java");
-        dataSource.setPassword("password");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/todo_list");
-        return dataSource;
-    }
-
-    /*
-    * i don't really know what this function is really
-    * */
-    @Bean(name = {"transactionManager", "hibernateTransactionManager", "PlatformTransactionManager"})
-    public PlatformTransactionManager hibernateTransactionManager() {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory().getObject());        
-        return transactionManager;    
-    }    
-
-    /*
-    * hibernate properties like auto creating database if not exist and more properties
-    * */
-    private Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();       
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
-        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5InnoDBDialect");
-        hibernateProperties.setProperty("show-sql", "true");
-        return hibernateProperties;  
-    }
 }
